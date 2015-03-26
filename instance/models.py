28ba5352432d628903338938e303410c873e519c
Boris Savelev <boris.savelev@gmail.com>
2015-03-26 12:07:04 +0300

    increase name field and create it uniq

--- instance/models.py
+++ instance/models.py
@@ -4,7 +4,7 @@ from servers.models import Compute
 
 class Instance(models.Model):
     compute = models.ForeignKey(Compute)
-    name = models.CharField(max_length=20)
+    name = models.CharField(max_length=255, unique=True)
     uuid = models.CharField(max_length=36)
     # display_name = models.CharField(max_length=50)
     # display_description = models.CharField(max_length=255)
